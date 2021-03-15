db1dddba67677a059be165ddacfb7e83f0fb22fc
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-03-15 12:31:08 +0800

    update

---
+++ --cppcode/cmake-build-debug/CMakeFiles/__cppcode.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/__cppcode.dir/main.cpp.obj"
+  "__cppcode.exe"
+  "__cppcode.exe.manifest"
+  "__cppcode.pdb"
+  "lib__cppcode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/__cppcode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- --javacode/javaprac/src/CH02/test2.java
+++ --javacode/javaprac/src/CH02/test2.java
@@ -4,9 +4,8 @@ public class test2 {
 
 	public static void main(String[] args) {
 // TODO Auto-generated method stub
-		System.out.println("x");
-		System.out.println("ss");
-		System.out.println("xx");
+		double x = 0.1+0.1+0.1;
+		System.out.println(x);
 	}
 
 }
