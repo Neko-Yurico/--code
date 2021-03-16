4037ad1363988f1b38c77bee09c4e706436db2e7
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-03-16 15:37:44 +0800

    1

--- --javacode/JavaProject/src/ch02/ScoreStatistics.java
+++ --javacode/JavaProject/src/ch02/ScoreStatistics.java
@@ -13,8 +13,16 @@ public class ScoreStatistics
 {
     public static void main(String[] args)
     {
-        int [] a= new int[]{91, 94, 84, 85, 27, 84, 54, 97, 48, 78, 86, 35};
-        
-
+        //原始数据
+        int[] scores = {72,89,65,58,87,91,53,82,71,93,76,68};
+        //求均值
+        double sum = 0;
+        for (int score : scores) {
+            sum = sum + score;
+        }
+        //输出
+        System.out.println("平均成绩为" + sum/scores.length );
+        System.out.printf("平均成绩为%.1f",sum/scores.length);
     }
+
 }
