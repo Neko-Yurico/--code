efa3c1d8480ba9331d2507c6926839865b66cb5c
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-03-16 13:04:29 +0800

    updated

--- --javacode/JavaProject/src/ch02/Prac2_3.java
+++ --javacode/JavaProject/src/ch02/Prac2_3.java
@@ -1,13 +1,14 @@
 package ch02;
 
-public class Prac2_3 {
+public class Prac2_3
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
 		System.out.println(5/3);
-		System.out.println((Double)5/3);
+		System.out.println((double)(5/3));
 		System.out.println(5.0/3);
-		System.out.println((int)5.0/3);
+		System.out.println((int)(5.0/3));
 	}
 
 }
