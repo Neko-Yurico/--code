5325837066e674f0a3c872458ee522e47ba4ac70
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-03-16 15:21:32 +0800

    Create ScoreStatistics.java

---
+++ --javacode/JavaProject/src/ch02/ScoreStatistics.java
@@ -0,0 +1,20 @@
+package ch02;
+
+/**
+ * @author Neko_Yurico
+ * @description
+ * @date 2021/3/16 15:07
+ * ========================
+ * WELCOME TO MY WEBSITE
+ * https://nekoyurico.me/
+ * ========================
+ */
+public class ScoreStatistics
+{
+    public static void main(String[] args)
+    {
+        int [] a= new int[]{91, 94, 84, 85, 27, 84, 54, 97, 48, 78, 86, 35};
+        
+
+    }
+}
