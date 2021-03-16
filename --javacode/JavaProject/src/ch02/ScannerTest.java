f6f586360ad30edfbc2435f73328e7d5a33e3509
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-03-16 14:13:25 +0800

    updated

---
+++ --javacode/JavaProject/src/ch02/ScannerTest.java
@@ -0,0 +1,30 @@
+package ch02;
+
+import java.util.Scanner;
+
+/**
+ * @author Neko_Yurico
+ * @description
+ * @date 2021/3/16 14:04
+ * ========================
+ * WELCOME TO MY WEBSITE
+ * https://nekoyurico.me/
+ * ========================
+ */
+public class ScannerTest {
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        int sum=0;
+        int m=0;
+        while (scanner.hasNextInt())
+        {
+            int p=scanner.nextInt();
+            sum+=p;
+            m=m+1;
+        }
+        System.out.printf("共输入了%d个数，和为%d。 \n",m,sum);
+        System.out.printf("平均值为%.1f",1.0*sum/m);
+
+    }
+}
