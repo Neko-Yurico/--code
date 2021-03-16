21f08e11783dad3cfb25e0899ca752e22a5babb5
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-03-16 14:26:45 +0800

    1

--- --javacode/JavaProject/src/ch02/ScannerTest.java
+++ --javacode/JavaProject/src/ch02/ScannerTest.java
@@ -15,12 +15,13 @@ public class ScannerTest {
     public static void main(String[] args)
     {
         System.out.println("请输入值，并使用回车连续输入值，使用ctrl+d结束输入：");
+        System.out.print("请输入第1个值:");
         Scanner scanner=new Scanner(System.in);
         int sum=0;
         int m=0;
         while (scanner.hasNextInt())
         {
-            System.out.printf("请输入第%d个值：",m+1);
+            System.out.printf("请输入第%d个值：",m+2);
             int p=scanner.nextInt();
             sum+=p;
             m=m+1;
