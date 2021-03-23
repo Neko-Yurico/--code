5dd33775bd23c473d746fcc091c8f6674f10f061
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-03-23 14:29:53 +0800

    1

--- --javacode/JavaProject/src/ch02/BMICalculate.java
+++ --javacode/JavaProject/src/ch02/BMICalculate.java
@@ -1,5 +1,7 @@
 package ch02;
 
+import java.util.Scanner;
+
 /**
  * @author Neko_Yurico
  * @description
@@ -11,5 +13,33 @@ package ch02;
  */
 public class BMICalculate
 {
-
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("请输入身高(m)：");
+        double height = scanner.nextDouble();
+        System.out.print("请输入体重(kg)：");
+        double weight = scanner.nextDouble();
+        double bmi = weight / (height * height);
+        if(bmi < 18.5)
+        {
+            System.out.println("过轻");
+        }
+        else if (bmi < 25 & !(bmi < 18.5))
+        {
+            System.out.println("正常");
+        }
+        else if (bmi < 28 & !(bmi < 25))
+        {
+            System.out.println("过重");
+        }
+        else if (bmi < 32 & !(bmi < 28))
+        {
+            System.out.println("肥胖");
+        }
+        else
+        {
+            System.out.println("非常肥胖");
+        }
+    }
 }
