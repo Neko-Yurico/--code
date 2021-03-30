52e66d878f1b23eb838b7666c2ca981de206c16a
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-03-30 14:43:22 +0800

    1

--- --javacode/JavaProject/src/ch02/StringTest.java
+++ --javacode/JavaProject/src/ch02/StringTest.java
@@ -1,6 +1,7 @@
 package ch02;
 
 import java.nio.charset.StandardCharsets;
+import java.util.SimpleTimeZone;
 
 /**
  * @author Neko_Yurico
@@ -23,5 +24,15 @@ public class StringTest
         System.out.println(as.length );
         byte[] abs = a.getBytes(StandardCharsets.UTF_8);
         System.out.println(abs.length );
+        String s= "wdt=44,g=5";
+        char[] s1 = s.toCharArray();
+        for (int i = 0; i < s1.length; i++)
+        {
+            if (Character.isDigit(s1[i]))
+            {
+                System.out.printf("在%d处找到字符%c\n",i,s1[i]);
+            }
+        }
     }
 }
+
