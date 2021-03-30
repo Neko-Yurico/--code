ec27347b57443e798ed7617d6fbe433a2c20d641
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-03-30 13:54:21 +0800

    1

--- --javacode/JavaProject/src/ch02/TestCode.java
+++ --javacode/JavaProject/src/ch02/TestCode.java
@@ -15,8 +15,12 @@ public class TestCode
 {
     public static void main(String[] args)
     {
-        String a =new String("hello") ;
-        String b =new String("hello") ;
+/**
+ *  String a =new String("hello") ;
+ *  String b =new String("hello") ;
+*/
+        String a = "hello";
+        String b = "hello";
         System.out.println(a==b);
         System.out.println(a.equals(b) );
     }
