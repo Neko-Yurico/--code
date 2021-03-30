51829663ef842f210786014c58588cfb71b59f57
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-03-30 14:04:27 +0800

    1

---
+++ --javacode/JavaProject/src/ch02/StringTest.java
@@ -0,0 +1,19 @@
+package ch02;
+
+/**
+ * @author Neko_Yurico
+ * @description
+ * @date 2021/3/30 14:03
+ * ========================
+ * WELCOME TO MY WEBSITE
+ * https://nekoyurico.me/
+ * ========================
+ */
+public class StringTest
+{
+    public static void main(String[] args)
+    {
+        String a=new String("hello你好") ;
+        System.out.println(a.length());
+    }
+}
