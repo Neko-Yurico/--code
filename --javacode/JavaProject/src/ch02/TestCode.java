99e2cbe63d509e853a0d2b34210af295b1267f35
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-03-30 14:01:48 +0800

    Update TestCode.java

--- --javacode/JavaProject/src/ch02/TestCode.java
+++ --javacode/JavaProject/src/ch02/TestCode.java
@@ -15,12 +15,8 @@ public class TestCode
 {
     public static void main(String[] args)
     {
-/**
- *  String a =new String("hello") ;
- *  String b =new String("hello") ;
-*/
-        String a = "hello";
-        String b = "hello";
+        String a =new String("hello") ;
+        String b =new String("hello") ;
         System.out.println(a==b);
         System.out.println(a.equals(b) );
     }
