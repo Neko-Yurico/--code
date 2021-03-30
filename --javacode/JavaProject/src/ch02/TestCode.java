3e3437ffa1e082e02cc223f3e72173619c95b074
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-03-30 13:49:59 +0800

    1

---
+++ --javacode/JavaProject/src/ch02/TestCode.java
@@ -0,0 +1,23 @@
+package ch02;
+
+import com.sun.tools.javac.Main;
+
+/**
+ * @author Neko_Yurico
+ * @description
+ * @date 2021/3/30 13:46
+ * ========================
+ * WELCOME TO MY WEBSITE
+ * https://nekoyurico.me/
+ * ========================
+ */
+public class TestCode
+{
+    public static void main(String[] args)
+    {
+        String a =new String("hello") ;
+        String b =new String("hello") ;
+        System.out.println(a==b);
+        System.out.println(a.equals(b) );
+    }
+}
