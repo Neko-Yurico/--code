961dc939fba3fa75bc2d8e304983f7b5a568d9e9
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-04-06 13:17:54 +0800

    Create EX02.java

---
+++ --javacode/javaexp128/src/ex02/EX02.java
@@ -0,0 +1,18 @@
+package ex02;
+
+/**
+ * @author Neko_Yurico
+ * @description
+ * @date 2021/4/6 13:15
+ * ========================
+ * WELCOME TO MY WEBSITE
+ * https://nekoyurico.me/
+ * ========================
+ */
+public class EX02
+{
+    public static void main(String[] args)
+    {
+        System.out.println("1");
+    }
+}
