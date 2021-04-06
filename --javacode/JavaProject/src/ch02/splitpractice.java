23097179a3697ceccc22c81fb316771712130f66
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-04-06 14:23:53 +0800

    1

--- --javacode/JavaProject/src/ch02/splitpractice.java
+++ --javacode/JavaProject/src/ch02/splitpractice.java
@@ -1,5 +1,9 @@
 package ch02;
 
+import com.sun.source.tree.WhileLoopTree;
+
+import java.util.Scanner;
+
 /**
  * @author Neko_Yurico
  * @description
@@ -12,11 +16,19 @@ package ch02;
 public class splitpractice {
     public static void main(String[] args) {
         String str = "T=12.3 H=058 L=007";
-        String[] ss = str.split("\\D*=") ;
-        System.out.println("找到"+ss.length +"个字符");
-        for (String string : ss )
-        {
-            System.out.println(string);
+/*
+       String[] ss = str.split("\\D*=") ;
+       System.out.println("找到"+ss.length +"个字符");
+       for (int i=1;i< ss.length ;i++ ) {
+           System.out.println(ss[i]);
+           }
+ */
+        Scanner scanner =new Scanner(str) ;
+        scanner .useDelimiter("[^0-9.]+") ;
+        double s=0;
+        while (scanner .hasNext() ){
+            s += Double.parseDouble(scanner.next());
         }
+        System.out.println("数据总和为"+s);
     }
 }
