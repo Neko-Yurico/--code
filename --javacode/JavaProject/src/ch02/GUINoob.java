cd3c1d26b3cbe645eb301fc7adaccda02ef4ddfb
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-04-25 16:04:09 +0800

    1

---
+++ --javacode/JavaProject/src/ch02/GUINoob.java
@@ -0,0 +1,18 @@
+package ch02;
+
+import javax.swing.*;
+
+/**
+ * @author Neko_Yurico
+ * @description
+ * @date 2021/4/25 15:57
+ * ========================
+ * WELCOME TO MY WEBSITE
+ * https://nekoyurico.me/
+ * ========================
+ */
+public class GUINoob {
+    JLabel disp =new JLabel ("这是文本标签") ;
+    JTextField input = new JTextField ("我是输入框");
+    JButton button =new JButton ("按钮") ;
+}
