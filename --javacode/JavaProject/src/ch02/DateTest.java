2fa81c319036f3f51293f8a97e1e045e32d43ea3
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-04-25 14:54:54 +0800

    1

--- --javacode/JavaProject/src/ch02/DateTest.java
+++ --javacode/JavaProject/src/ch02/DateTest.java
@@ -2,6 +2,7 @@ package ch02;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Timer;
 
 /**
  * @author Neko_Yurico
@@ -14,13 +15,18 @@ import java.util.Date;
  */
 public class DateTest {
     public static void main ( String[] args ) {
-        do {
-            Date date = new Date ( );
-            System.out.println ( date );
-            SimpleDateFormat sdf = new SimpleDateFormat ( "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n今年是yyyy年MM月dd日 E\naHH时mm分ss秒" );
-            //清奇脑回路源于https://blog.csdn.net/ywh22122/article/details/98765764
-            String f = sdf.format ( new Date ( ) );
-            System.out.println ( f );
-        }while(true );
+        while ( true ) {
+            try {
+                Date date = new Date ( );
+                System.out.println ( date );
+                SimpleDateFormat sdf = new SimpleDateFormat ( "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n今年是yyyy年MM月dd日 E\naHH时mm分ss秒\n\n\n\n" );
+                //清奇脑回路源于`https://blog.csdn.net/ywh22122/article/details/98765764`
+                String f = sdf.format ( new Date ( ) );
+                System.out.println ( f );
+                Thread.sleep ( 1000 );
+            } catch (InterruptedException e) {
+                e.printStackTrace ( );
+            }
+        }
     }
 }
