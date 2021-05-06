67622fb4df4e584c3283f793ea398c3fa48cf59b
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-05-06 12:10:25 +0800

    update

--- --javacode/demo/src/me/nekoyurico/project/Communication.java
+++ --javacode/demo/src/me/nekoyurico/project/Communication.java
@@ -1,5 +1,7 @@
 package me.nekoyurico.project;
 
+import com.fazecast.jSerialComm.SerialPort;
+
 /**
  * @author Neko_Yurico，相欠
  * @description Communicate with the board
@@ -16,11 +18,76 @@ package me.nekoyurico.project;
  * |  |    |  |  \       / |  |  \    /   '. \_/``".'  (_{;}_) (_(=)_) \      /       \ /  . \ /|  |  \    / |   |  `-'`-'     /  '. \_/``".'
  * '--'    '--'   `'-..-'  `--'   `'-'      '-----'   --(_,_)---(_I_)-  `-..-'         ``-'`-'' ''-'   `'-'  '---'    `._____.'     '-----'
  * <p>
+ *   　　　　　　　　＃＃　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　＃　　　　　　　　　　　　　　　　　
+ * 　　　　　　　　　＃＃　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　＃＃＃　　　　　　　　　　　　　　　　
+ * 　　　　　　　　　＃＃　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　＃＃＃　　　　　　　　　　　　　　　　
+ * 　　　　　　　　　＃＃　　　＃＃　　　　　　＃＃　　　　　　　　　　　　　　　　　＃＃　　　　　　　　　　　　　　　　　
+ * 　　　　　　　　　＃＃　　　＃＃　　　＃＃＃＃＃＃　　　　　　　　　　　　　　　＃＃＃　　　　　　　　　　　　　　　　　
+ * 　　　　　　　　　＃＃　　　＃＃＃＃＃＃＃＃＃＃　　　　　　　　　　　　　　　　＃＃　　　　　　　　　＃＃　　　　　　　
+ * 　　　　　　　　　＃＃　　　　＃＃＃＃　　　＃＃　　　　　　　　　　　　　　　　＃＃　　　　　＃＃＃＃＃＃＃　　　　　　
+ * 　　　　　　　＃＃＃＃＃＃＃＃＃　　　　　　＃＃　　　　　　　　　　　　　　　＃＃＃＃＃＃＃＃＃＃＃＃＃＃　　　　　　　
+ * 　　　＃＃＃＃＃＃＃＃＃＃＃＃＃　　　　　　＃＃　　　　　　　　　　　　　　　＃＃＃＃＃＃＃＃　　＃＃＃　　　　　　　　
+ * 　　　　＃＃＃　＃＃　　　　　＃　　＃＃＃＃＃＃　　　　　　　　　　　　　　＃＃　　　＃＃　　　　＃＃　　　　　　　　　
+ * 　　　　　　　＃＃＃＃＃　　　＃＃＃＃＃＃＃＃＃　　　　　　　　　　　　　＃＃＃　　　　＃＃　　＃＃　　　　　　　　　　
+ * 　　　　　　　＃＃＃＃＃＃　　＃＃＃　　　　＃＃　　　　　　　　　　　　　＃＃　　　　＃＃＃　　　　　　　　　　　　　　
+ * 　　　　　　＃＃＃＃＃＃＃＃　＃　　　　　　＃　　　　　　　　　　　　　＃＃　　　　　＃＃　　　　　　　　　　　　　　　
+ * 　　　　　　＃＃　＃＃　＃＃　＃　　　　　＃＃　　　　　　　　　　　　＃＃　　　　　　＃＃＃　　　　　　　　　　　　　　
+ * 　　　　　＃＃＃　＃＃　　＃　＃＃＃＃＃＃＃＃　　　　　　　　　　　＃＃　　　　　　＃＃＃＃　　　　　　　　　　　　　　
+ * 　　　　　＃＃　　＃＃　　　　＃＃＃＃＃＃＃＃　　　　　　　　　　　　　　　　　　　＃＃　＃＃　　　　　　　　　　　　　
+ * 　　　　＃＃　　　＃＃　　　　＃　　　　　＃＃　　　　　　　　　　　　　　　　　　＃＃＃　＃＃＃　　　　　　　　　　　　
+ * 　　　＃＃＃　　　＃＃　　　　＃　　　　　＃＃　　　　　　　　　　　　　　　　　　＃＃　　　＃＃　　　　　　　　　　　　
+ * 　　　＃＃　　　　＃＃　　　　＃　　　　　＃＃　　　　　　　　　　　　　　　　　＃＃＃　　　　＃＃　　　　　　　　　　　
+ * 　　　＃　　　　　＃＃　　　　＃＃＃＃＃＃＃＃　　　　　　　　　　　　　　　　　＃＃　　　　　＃＃＃　　　　　　　　　　
+ * 　　　　　　　　　＃＃　　　　＃＃＃＃＃＃＃＃　　　　　　　　　　　　　　　　＃＃　　　　　　　＃＃＃　　　　　　　　　
+ * 　　　　　　　　＃＃＃　　　＃＃＃　　　　＃＃　　　　　　　　　　　　　　　＃＃　　　　　　　　　＃＃＃　　　　　　　　
+ * 　　　　　　　　＃＃　　　　＃＃　　　　　＃＃　　　　　　　　　　　　　＃＃＃　　　　　　　　　　　＃＃＃＃＃＃　　　　
+ * 　　　　　　　　　＃　　　　　＃　　　　　　　　　　　　　　　　　　＃＃＃＃　　　　　　　　　　　　＃＃＃＃＃＃　　　　
+ * 　　　　　　　　　＃　　　　　　　　　　　　　　　　　　　　　　　＃＃＃　　　　　　　　　　　　　　　　　　　　　　　　
+ * <p>
  * ========================
  * WELCOME TO MY WEBSITE
  * https://nekoyurico.me/
  * ========================
  */
-public class Communication { //与开发板通信
+public class Communication {
 
+
+    public Communication ( String ComPort ) {
+        super ( );
+        // 初始化串口
+        // blink,and set all off
+        // 读取初始状态
+        // ledStatus=readLeds();
+    }
+    public static void main ( String[] args ) {
+        byte ledAllOff = 0x00;
+        byte ledAllOn = 0x07;
+        byte[] bytes = { ledAllOff };
+        // private SerialPort[] allPorts;
+        SerialPort sp;
+        sp = SerialPort.getCommPort ( "COM8" );
+        sp.setBaudRate ( 9600 );
+        // test whether port is open
+        if ( ! sp.isOpen ( ) ) {
+            sp.openPort ( );
+        }
+        try {
+            // all on,and set all off
+            bytes[ 0 ] = ledAllOn;
+            sp.writeBytes ( bytes , 1 );
+            Thread.sleep ( 10 );
+            bytes[ 0 ] = ledAllOff;
+            sp.writeBytes ( bytes , 1 );
+            Thread.sleep ( 500 );
+            // seperate control
+            bytes[ 0 ] = 0x11;
+            sp.writeBytes ( bytes , 1 );
+            Thread.sleep ( 2000 );
+            bytes[ 0 ] = 0x10;
+            sp.writeBytes ( bytes , 1 );
+        } catch ( InterruptedException e ) {
+            // TODO Auto-generated catch block
+            e.printStackTrace ( );
+        }
+    }
 }
--- --javacode/demo/src/me/nekoyurico/project/UI.java
+++ --javacode/demo/src/me/nekoyurico/project/UI.java
@@ -1,5 +1,13 @@
 package me.nekoyurico.project;
 
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 /**
  * @author Neko_Yurico，相欠
  * @description UI
@@ -16,11 +24,72 @@ package me.nekoyurico.project;
  * |  |    |  |  \       / |  |  \    /   '. \_/``".'  (_{;}_) (_(=)_) \      /       \ /  . \ /|  |  \    / |   |  `-'`-'     /  '. \_/``".'
  * '--'    '--'   `'-..-'  `--'   `'-'      '-----'   --(_,_)---(_I_)-  `-..-'         ``-'`-'' ''-'   `'-'  '---'    `._____.'     '-----'
  * <p>
+ *   　　　　　　　　＃＃　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　＃　　　　　　　　　　　　　　　　　
+ * 　　　　　　　　　＃＃　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　＃＃＃　　　　　　　　　　　　　　　　
+ * 　　　　　　　　　＃＃　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　＃＃＃　　　　　　　　　　　　　　　　
+ * 　　　　　　　　　＃＃　　　＃＃　　　　　　＃＃　　　　　　　　　　　　　　　　　＃＃　　　　　　　　　　　　　　　　　
+ * 　　　　　　　　　＃＃　　　＃＃　　　＃＃＃＃＃＃　　　　　　　　　　　　　　　＃＃＃　　　　　　　　　　　　　　　　　
+ * 　　　　　　　　　＃＃　　　＃＃＃＃＃＃＃＃＃＃　　　　　　　　　　　　　　　　＃＃　　　　　　　　　＃＃　　　　　　　
+ * 　　　　　　　　　＃＃　　　　＃＃＃＃　　　＃＃　　　　　　　　　　　　　　　　＃＃　　　　　＃＃＃＃＃＃＃　　　　　　
+ * 　　　　　　　＃＃＃＃＃＃＃＃＃　　　　　　＃＃　　　　　　　　　　　　　　　＃＃＃＃＃＃＃＃＃＃＃＃＃＃　　　　　　　
+ * 　　　＃＃＃＃＃＃＃＃＃＃＃＃＃　　　　　　＃＃　　　　　　　　　　　　　　　＃＃＃＃＃＃＃＃　　＃＃＃　　　　　　　　
+ * 　　　　＃＃＃　＃＃　　　　　＃　　＃＃＃＃＃＃　　　　　　　　　　　　　　＃＃　　　＃＃　　　　＃＃　　　　　　　　　
+ * 　　　　　　　＃＃＃＃＃　　　＃＃＃＃＃＃＃＃＃　　　　　　　　　　　　　＃＃＃　　　　＃＃　　＃＃　　　　　　　　　　
+ * 　　　　　　　＃＃＃＃＃＃　　＃＃＃　　　　＃＃　　　　　　　　　　　　　＃＃　　　　＃＃＃　　　　　　　　　　　　　　
+ * 　　　　　　＃＃＃＃＃＃＃＃　＃　　　　　　＃　　　　　　　　　　　　　＃＃　　　　　＃＃　　　　　　　　　　　　　　　
+ * 　　　　　　＃＃　＃＃　＃＃　＃　　　　　＃＃　　　　　　　　　　　　＃＃　　　　　　＃＃＃　　　　　　　　　　　　　　
+ * 　　　　　＃＃＃　＃＃　　＃　＃＃＃＃＃＃＃＃　　　　　　　　　　　＃＃　　　　　　＃＃＃＃　　　　　　　　　　　　　　
+ * 　　　　　＃＃　　＃＃　　　　＃＃＃＃＃＃＃＃　　　　　　　　　　　　　　　　　　　＃＃　＃＃　　　　　　　　　　　　　
+ * 　　　　＃＃　　　＃＃　　　　＃　　　　　＃＃　　　　　　　　　　　　　　　　　　＃＃＃　＃＃＃　　　　　　　　　　　　
+ * 　　　＃＃＃　　　＃＃　　　　＃　　　　　＃＃　　　　　　　　　　　　　　　　　　＃＃　　　＃＃　　　　　　　　　　　　
+ * 　　　＃＃　　　　＃＃　　　　＃　　　　　＃＃　　　　　　　　　　　　　　　　　＃＃＃　　　　＃＃　　　　　　　　　　　
+ * 　　　＃　　　　　＃＃　　　　＃＃＃＃＃＃＃＃　　　　　　　　　　　　　　　　　＃＃　　　　　＃＃＃　　　　　　　　　　
+ * 　　　　　　　　　＃＃　　　　＃＃＃＃＃＃＃＃　　　　　　　　　　　　　　　　＃＃　　　　　　　＃＃＃　　　　　　　　　
+ * 　　　　　　　　＃＃＃　　　＃＃＃　　　　＃＃　　　　　　　　　　　　　　　＃＃　　　　　　　　　＃＃＃　　　　　　　　
+ * 　　　　　　　　＃＃　　　　＃＃　　　　　＃＃　　　　　　　　　　　　　＃＃＃　　　　　　　　　　　＃＃＃＃＃＃　　　　
+ * 　　　　　　　　　＃　　　　　＃　　　　　　　　　　　　　　　　　　＃＃＃＃　　　　　　　　　　　　＃＃＃＃＃＃　　　　
+ * 　　　　　　　　　＃　　　　　　　　　　　　　　　　　　　　　　　＃＃＃　　　　　　　　　　　　　　　　　　　　　　　　
+ * <p>
  * ========================
  * WELCOME TO MY WEBSITE
  * https://nekoyurico.me/
  * ========================
  */
-public class UI { //用户界面
 
+@SuppressWarnings ( "serial" )
+public class UI extends JFrame {
+    public final int ledCNT = 3;
+    private boolean ledStatus = false;
+    public UI ( ) {
+        Communication lights = new Communication ( "COM8" );
+        ImageIcon imageOFF = new ImageIcon ( "led_off.jpg" );
+        ImageIcon imageON = new ImageIcon ( "led_on.jpg" );
+        // using JButton
+        JButton leds = new JButton ( imageOFF );
+        leds.addActionListener ( new ActionListener ( ) {
+            @Override
+            public void actionPerformed ( ActionEvent e ) {
+                if ( ledStatus ) {
+                    leds.setIcon ( imageOFF );
+                    //lights.setOneLed(1, 0);
+                    ledStatus = false;
+                }
+                else {
+
+                    leds.setIcon ( imageON );
+                    //lights.setOneLed(1, 1);
+                    ledStatus = true;
+                }
+            }
+        } );
+        // JFrame基本结构
+        setTitle ( "LED控制" );
+        setBounds ( 500 , 300 , 330 , 370 );
+        setVisible ( true );
+        setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
+        add ( leds );
+    }
+    public static void main ( String[] args ) {
+        new UI ( );
+    }
 }
