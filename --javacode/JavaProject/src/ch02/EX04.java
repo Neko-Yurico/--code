d73c02bc0db57243854e80fa49e24264a8c4dfd0
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-05-11 13:54:14 +0800

    1

---
+++ --javacode/JavaProject/src/ch02/EX04.java
@@ -0,0 +1,28 @@
+package ch02;
+
+import java.util.Scanner;
+
+/**
+ * @author Neko_Yurico
+ * @description
+ * @date 2021/4/27 11:27
+ * ========================
+ * WELCOME TO MY WEBSITE
+ * https://nekoyurico.me/
+ * ========================
+ */
+public class EX04 {
+    public static void main ( String[] args ) {
+        System.out.println ( "请输入一个运算表达式（例如sin-5):" );
+        Scanner scanner = new Scanner ( System.in );
+        String s = scanner.next ( );
+        s = s.trim ( );
+        while ( ! s.equals ( "q" ) && ! s.equals ( "Q" ) ) {
+            Fx f = new Fx ( s );
+            System.out.println ( f.getX ( ) + "\n请输入下一个算数表达式,退出请输入“q”：" );
+            s = scanner.next ( );
+            s = s.trim ( );
+        }
+        System.out.println ( "检测到输入为“q”，程序退出结束" );
+    }
+}
--- --javacode/JavaProject/src/ch02/MathOpGui.java
+++ --javacode/JavaProject/src/ch02/MathOpGui.java
@@ -1,13 +1,10 @@
-package me.nekoyurico.project;
-
-import javax.swing.*;
+package ch02;
 
 /**
- * @author Neko_Yurico,BOW
+ * @author Neko_Yurico
  * @description
- * @date 2021/5/6 12:35
+ * @date 2021/5/11 13:47
  * ========================
- * <p>
  * ,---.   .--.    .-''-.  .--.   .--.      ,-----.                      ____     __   ___    _ .-------.   .-./`)     _______      ,-----.
  * |    \  |  |  .'_ _   \ |  | _/  /     .'  .-,  '.                    \   \   /  /.'   |  | ||  _ _   \  \ .-.')   /   __  \   .'  .-,  '.
  * |  ,  \ |  | / ( ` )   '| (`' ) /     / ,-.|  \ _ \                    \  _. /  ' |   .'  | || ( ' )  |  / `-' \  | ,_/  \__) / ,-.|  \ _ \
@@ -17,34 +14,14 @@ import javax.swing.*;
  * |  (_,_)\  | \  `-'    /|  | \ `'   / \ `"/  \  ) /--( ' )---(_I_)-|   `-'  /     | (_ (_) _)|  | \ `'   /|   | (  .  .-'_/  )\ `"/  \  ) /
  * |  |    |  |  \       / |  |  \    /   '. \_/``".'  (_{;}_) (_(=)_) \      /       \ /  . \ /|  |  \    / |   |  `-'`-'     /  '. \_/``".'
  * '--'    '--'   `'-..-'  `--'   `'-'      '-----'   --(_,_)---(_I_)-  `-..-'         ``-'`-'' ''-'   `'-'  '---'    `._____.'     '-----'
- * <p>
- * d8888b.  .d88b.  db   d8b   db
- * 88  `8D .8P  Y8. 88   I8I   88
- * 88oooY' 88    88 88   I8I   88
- * 88~~~b. 88    88 Y8   I8I   88
- * 88   8D `8b  d8' `8b d8'8b d8'
- * Y8888P'  `Y88P'   `8b8' `8d8'
- * <p>
  * ========================
  * WELCOME TO MY WEBSITE
  * https://nekoyurico.me/
  * ========================
  */
-
-public class UI {
-
+public class MathOpGui {
     public static void main ( String[] args ) {
-        JFrame frame = new JFrame ( "UI" );
-        frame.setContentPane ( new UI ( ).panel1 );
-        frame.setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
-        frame.pack ( );
-        frame.setVisible ( true );
+        double result = MathOp.eval ( "sin(1)" );
+        System.out.println ("sin(1)="+result );
     }
-
-    private JPanel panel1;
-    private JTable table1;
-    private JButton button1;
-    private JButton button2;
-    private JTextField textField1;
-
 }
