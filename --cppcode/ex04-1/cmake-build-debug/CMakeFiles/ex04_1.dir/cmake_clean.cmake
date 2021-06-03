237f8d4c1ed31f3ccaa2352d95f193030b1156cf
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-06-03 13:31:02 +0800

    1

---
+++ --cppcode/ex04-1/cmake-build-debug/CMakeFiles/ex04_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex04_1.dir/main.cpp.obj"
+  "ex04_1.exe"
+  "ex04_1.exe.manifest"
+  "ex04_1.pdb"
+  "libex04_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex04_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
