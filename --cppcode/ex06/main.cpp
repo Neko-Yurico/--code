05fdb5f0283a97ee8408963f050879f439ca6d70
“945998493@qq.com” <55744641+Neko-Yurico@users.noreply.github.com>
2021-06-17 14:53:27 +0800

    1

--- --cppcode/ex06/main.cpp
+++ --cppcode/ex06/main.cpp
@@ -1,59 +1,50 @@
-//#include <iostream>
-//#include <fstream>
-//#include <string>
-//using namespace std;
-//class message{
-//public:
-//    char msg;
-//    friend ostream & operator <<(ostream &out,message &s);
-//    friend istream & operator >>(istream &in,message &s);
-//};
-//
-//ostream &operator<<(ostream &out, message &s) {
-//    out<<s.msg;
-//    return out;
-//}
-//
-//istream &operator>>(istream &in, message &s) {
-//    in>>s.msg;
-//    return in;
-//}
-//
-//int main() {
-//    message msg;
-//    string b;
-//    cout<<"Please choose whether to save the contents of the original file(y/n):";
-//    cin>>b;
-//    if (b =="y"){
-//        ifstream ifs("D:\\message.txt",ios::ate);
-//        cout<<"read the file:"<<endl;
-//        ifs>>msg;
-//        while (!ifs.eof()){
-//            cout<<msg;
-//            ifs>>msg;
-//        }
-//        cout<<"\n";
-//        ifs.close();
-//        ofstream ofs("D:\\message.txt",ios::ate);
-//        cout<<"please input the message:"<<endl;
-//        cin>>msg;
-//        ofs<<msg;
-//        ofs.close();
-//    } else {
-//        ifstream ifs("D:\\message.txt",ios::trunc);
-//        cout<<"read the file:"<<endl;
-//        ifs>>msg;
-//        while (!ifs.eof()){
-//            cout<<msg;
-//            ifs>>msg;
-//        }
-//        cout<<"\n";
-//        ifs.close();
-//        ofstream ofs("D:\\message.txt",ios::trunc);
-//        cout<<"please input the message:"<<endl;
-//        cin>>msg;
-//        ofs<<msg;
-//        ofs.close();
-//    }
-//
-//}
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+class message{
+public:
+    char msg[1000];
+    friend ostream & operator <<(ostream &out,message &s);
+    friend istream & operator >>(istream &in,message &s);
+};
+
+ostream &operator<<(ostream &out, message &s) {
+    out<<s.msg;
+    return out;
+}
+
+istream &operator>>(istream &in, message &s) {
+    in>>s.msg;
+    return in;
+}
+
+int main() {
+    ofstream ofile;
+    ifstream ifile;
+    ofile.open("d:\\message.txt",ios::app);
+    ofile.close();
+    message msg{};
+    string b;
+    cout<<"Read the file:"<<endl;
+    ifile.open("d:\\message.txt",ios::in);
+    while (!ifile.eof()){
+        ifile>>msg;
+    }
+    cout<<msg<<endl;
+    ifile.close();
+    cout<<"Please choose whether to save the contents of the original file(y/n):";
+    cin>>b;
+    cout<<"Please input your message:"<<endl;
+    if (b =="y"){
+        ofile.open("d:\\message.txt",ios::app);
+        cin>>msg;
+        ofile<<msg;
+        ofile.close();
+    } else {
+        ofile.open("d:\\message.txt",ios::trunc);
+        cin>>msg;
+        ofile<<msg;
+        ofile.close();
+    }
+}
